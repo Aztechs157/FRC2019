98dc0520178a9320b5a075b5a0d9c7579cb1f952
Aztechs1 <Aztechs1>
2019-02-05 19:31:50 -0500

    Outriggers with PID

--- src/main/java/org/usfirst/frc157/FRC2019/Robot.java
+++ src/main/java/org/usfirst/frc157/FRC2019/Robot.java
@@ -106,7 +106,7 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        outriggers.outriggers1.setPosition(0);
+        outriggers.outriggers1.tare();
         if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
