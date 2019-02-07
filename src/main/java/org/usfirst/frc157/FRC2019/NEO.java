8553a5fdc330c6a719c65103da59fc3380065436
Aztechs1 <Aztechs1>
2019-02-06 20:42:12 -0500

    Added error handling for NEO class

--- src/main/java/org/usfirst/frc157/FRC2019/NEO.java
+++ src/main/java/org/usfirst/frc157/FRC2019/NEO.java
@@ -3,13 +3,22 @@ package org.usfirst.frc157.FRC2019;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.hal.util.UncleanStatusException;
+
 
 public class NEO {
     private CANSparkMax motor;
     private double ticks;
     private double offset;
 	public NEO (int port, MotorType type) {
-        motor = new CANSparkMax(7, MotorType.kBrushless);
+        try
+        {
+            motor = new CANSparkMax(port, MotorType.kBrushless);
+        }
+        catch (UncleanStatusException e) 
+        {
+            System.err.println("ERROR: Spark failed at: " + port + " check connections");
+        }
         ticks = 0;
         offset = 0;
     }
@@ -18,17 +27,29 @@ public class NEO {
         return ticks-offset;
     }
     public void tare() {
-        ticks = motor.getEncoder().getPosition();
-        offset = ticks;
+        try {
+            ticks = motor.getEncoder().getPosition();
+            offset = ticks;
+        } catch (NullPointerException e) {
+            System.err.println(e);
+        }
     }
     public void setPosition(double position) {
-        ticks = motor.getEncoder().getPosition();
-        offset = ticks - position;
+        try {
+            ticks = motor.getEncoder().getPosition();
+            offset = ticks - position;
+        } catch (NullPointerException e) {
+            System.err.println(e);
+        }
     }
     public double getVelocity() {
         return motor.getEncoder().getVelocity();
     }
     public void set(double speed) {
-        motor.set(speed);
+        try {
+            motor.set(speed);
+        } catch (NullPointerException e) {
+            System.err.println(e);
+        }
     }
 }
